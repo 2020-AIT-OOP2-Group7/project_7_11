e979ce834a27c091dbfc619d0750781cde0a5a2c
aminokousuke <75057442+aminokousuke@users.noreply.github.com>
2020-12-03 15:51:20 +0900

    監視プログラム１　網野
    image_process.pyがフォルダ監視
    test.pyはグレースケール化単体のプログラムです

--- image_process.py
+++ image_process.py
@@ -1,1 +1,25 @@
-#ディレクトリの監視
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import os
+import time
+import cv2
+import numpy as np
+
+
+class ChangeHandler(FileSystemEventHandler):
+    # すべてのイベント
+    def on_any_event(self, event):
+        print('[全て]',event)
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        print('%s' % filename)
+
+
+observer = Observer()
+# 監視するフォルダを第２引数に指定
+observer.schedule(ChangeHandler(), 'upload_images', recursive=True)
+# 監視を開始する
+observer.start()
+
+while True:
+    time.sleep(5)
