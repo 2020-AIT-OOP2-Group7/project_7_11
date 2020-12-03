b55857294a475fe8834b0ae48853f9f8cefb481f
tarte-351123 <k19021kk@aitech.ac.jp>
2020-12-03 14:59:07 +0900

    ディレクトリの監視ができるようになったよ(暫定版)

--- image_process.py
+++ image_process.py
@@ -1,1 +1,17 @@
 #ディレクトリの監視
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import time
+
+class ChangeHandler(FileSystemEventHandler):
+    #全てのイベント
+    def on_any_event(self, event):
+        print('ディレクトリが更新されました')
+
+observer = Observer()
+observer.schedule(ChangeHandler(), 'upload_images',recursive=True)
+observer.start()
+
+while True:
+    time.sleep(5)
