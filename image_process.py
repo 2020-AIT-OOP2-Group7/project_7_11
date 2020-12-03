0983eb9d13195e8f3506f8497542e6e46ece8674
katokiti <katokiti6101@gmail.com>
2020-12-03 15:13:57 +0900

    輪郭の検出

--- image_process.py
+++ image_process.py
@@ -1,1 +1,10 @@
-#ディレクトリの監視
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('ここに画像のファイル',0)
+edges = cv2.Canny(img,100,200)
+
+cv2.imshow('output', edges)
+cv2.imwrite("output.png", edges)
+cv2.waitKey(0)
