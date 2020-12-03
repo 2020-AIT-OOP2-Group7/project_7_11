3e98e39692fe4562c2771736780d7016078dd28e
MitsushigeFukumoto <69783204+MitsushigeFukumoto@users.noreply.github.com>
2020-12-03 15:22:13 +0900

    web.pyのアップロード画像の表示画面の追加

--- web.py
+++ web.py
@@ -4,6 +4,11 @@ from flask import Flask, request    # Flaskは必須、requestはリクエスト
 app = Flask(__name__)
 
 
+# アップロード画像の表示
+@app.route('/upload_image')
+def upload_image():
+    # アップロード画像の表示画面のテンプレートを呼び出し
+    return render_template('upload_image.html')
 
 if __name__ == "__main__":
     # debugモードが不要の場合は、debug=Trueを消してください
