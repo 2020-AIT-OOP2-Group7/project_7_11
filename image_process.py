7217d38d5ca9bba91d7fb953ef5d171055f6c142
aminokousuke <75057442+aminokousuke@users.noreply.github.com>
2020-12-09 17:37:47 +0900

    網野　監視、グレースケール化（多分）完成
    ループ部分はうまくできなかったので先生のやつのコピペになってます
    ギリギリになってしまいましたが確認してもらえると幸いです

--- image_process.py
+++ image_process.py
@@ -5,22 +5,33 @@ import time
 import cv2
 import numpy as np
 
-
 class ChangeHandler(FileSystemEventHandler):
     # すべてのイベント
     def on_any_event(self, event):
-        print('[全て]',event)
         filepath = event.src_path
         filename = os.path.basename(filepath)
-        print('%s' % filename)
+        print('%s を確認' % filename)
+        #グレイスケール化
+        gray(event.src_path)
 
+#グレイスケール化
+def gray(filepath):
+    im = cv2.imread(filepath)
+    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('grayscale_images/' + os.path.basename(filepath), im_gray)
+    print('%s をグレースケール化しました' % os.path.basename(filepath))
 
-observer = Observer()
-# 監視するフォルダを第２引数に指定
-observer.schedule(ChangeHandler(), 'upload_images', recursive=True)
-# 監視を開始する
-observer.start()
-
-while True:
-    time.sleep(5)
-
+if __name__ == '__main__':
+    while 1:
+        observer = Observer()
+        # 監視するフォルダを第２引数に指定
+        observer.schedule(ChangeHandler(), "upload_images", recursive=True)
+        # 監視を開始する
+        observer.start()
+        
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
